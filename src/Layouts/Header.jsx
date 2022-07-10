cc433039a9eef41804745ec6eb2a509f886f21ef
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-07-11 01:35:04 +0800

    router added

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,11 +1,20 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import React from "react";
 
 const Header = ()=>{
     return (
         <header>
-            <AppBar><Toolbar>WheresMyTr?</Toolbar></AppBar>
+            <AppBar>
+                <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    WhereisMyTR?
+                </Typography>
+                    <Button color="inherit">NTU Directory</Button>
+                </Toolbar>
+            </AppBar>
         </header>
     )
 }
